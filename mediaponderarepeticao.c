568c9caa10ad29756d88472a94f77f4f81f3b607
DouglasMiqueias <163609389+DouglasMiqueias@users.noreply.github.com>
2024-10-02 08:39:53 -0300

    Add files via upload

---
+++ mediaponderarepeticao.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, i;
+    double resultado = 0, a, b, c;
+
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%lf %lf %lf", &a, &b, &c);
+
+        resultado = (a * 2 + b * 3 + c * 5) / 10;
+
+        printf("%.1lf\n", resultado);
+    }
+
+    return 0;
+}
