6ca66f6cb5f3fdbd1440f87a7a6ed4894c9b340e
DouglasMiqueias <163609389+DouglasMiqueias@users.noreply.github.com>
2024-10-08 13:49:22 -0300

    Add files via upload

--- jogodomaior.c
+++ jogodomaior.c
@@ -1,30 +1,32 @@
-        #include<stdio.h>
-        int main()
-        {
-            int rodadas, i, v1, v2, aux1= 0,aux2= 0;
+#include <stdio.h>
 
-            scanf("%d", &rodadas);
+int main() {
+    int rodadas, i, v1, v2, aux1, aux2;
 
-            for(i = 0; i < rodadas; i++ )
-            {
-                scanf("%d%d", &v1, &v2);
-            if(0 < v1, v2 < 10)
-            {
+    while (1) {
+        scanf("%d", &rodadas);
 
-
-                if(v1>v2)
-                {
-                    aux1++;
-                }else {
-                    aux2++;
-                }
-                if(rodadas == 0)
-                {
-                    break;
-                }
-            }
-            }
-
-            printf("%d %d", aux1, aux2);
-            return 0;
+        if (rodadas == 0) {
+            break; // Encerra o programa se rodadas for 0
         }
+
+        aux1 = 0; // Reiniciar contadores para cada partida
+        aux2 = 0;
+
+        for (i = 0; i < rodadas; i++) {
+            scanf("%d %d", &v1, &v2);
+            if (v1 >= 0 && v1 <= 10 && v2 >= 0 && v2 <= 10) {
+                if (v1 > v2) {
+                    aux1++; // Primeiro jogador ganha
+                } else if (v1 < v2) {
+                    aux2++; // Segundo jogador ganha
+                }
+                // Se v1 == v2, ninguém ganha ponto
+            }
+        }
+
+        printf("%d %d\n", aux1, aux2); // Imprimir resultados após cada partida
+    }
+
+    return 0;
+}
