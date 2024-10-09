34a9dd7e9139f62e6b81a00799fb735115cccee8
DouglasMiqueias <163609389+DouglasMiqueias@users.noreply.github.com>
2024-10-08 21:29:27 -0300

    Update mediaponderarepeticao.c

--- mediaponderarepeticao.c
+++ mediaponderarepeticao.c
@@ -5,8 +5,8 @@ int main()
     int n, i;
     double resultado = 0, a, b, c;
 
-    scanf("%d", &n);
-    for (i = 0; i < n; i++)
+    scanf("%d", &n); 
+    for (i = 0; i < n; i++) // while(n--)
     {
         scanf("%lf %lf %lf", &a, &b, &c);
 
