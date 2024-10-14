0dd65e0cd2ba471de57bf865289830646454249a
DouglasMiqueias <163609389+DouglasMiqueias@users.noreply.github.com>
2024-10-14 16:55:02 -0300

    distancia.c

---
+++ distancia.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+   int distancia, min, x, y;
+   scanf("%d", &distancia);
+   x = distancia / 0.5;
+   printf("%d", x);
+    return 0;
+}
+// Velocidade relativa=Velocidade de Y−Velocidade de X=90−60=30 km/h
+// Tempo= distancia / velocidade
+// 30km/h = 30/60 = 0,5km/min
+// tempo = distancia / 0,5
