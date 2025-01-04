73e5b9a53e5264afc5c21f2f8ce91c9b8e3ffb6c
DouglasMiqueias <163609389+DouglasMiqueias@users.noreply.github.com>
2025-01-04 18:51:56 -0300

    Create 3432.cpp

---
+++ 3432.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int bits[8];
+
+    for (int i = 0; i < 8; ++i) {
+        cin >> bits[i];
+    }
+
+    for (int i = 0; i < 8; ++i) {
+        if (bits[i] == 9) {
+            cout << "F" << endl;
+            return 0;
+        }
+    }
+
+    cout << "S" << endl;
+    return 0;
+}
